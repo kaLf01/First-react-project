c93fd81b7cdaf2f890c59764d072dc3694144b49
mohamed <mohamedk24233@gmail.com>
2020-12-17 13:25:37 +0200

    first edit

--- src/App.js
+++ src/App.js
@@ -5,28 +5,34 @@ import 'antd/dist/antd.css'
 import { Layout, Menu, Breadcrumb, Card } from 'antd';
 import Card2 from './componants/card/myCard2'
 import myCalendar from './componants/calandar/Calandar'
-import React,{ useState } from 'react';
+// import Profile from './componants/profile/Profile'
+import Login from './componants/form/Login'
+import Register from './componants/form/Register'
+import React, { useState } from 'react';
 
 
 
 function App() {
   const { Header, Content, Footer } = Layout;
 
-  const[activeTap,setActiveTap] = useState(Card2);
+  const [activeTap, setActiveTap] = useState(Login);
 
   const switchTap = (e) => {
     switch (e.key) {
-      case 'profile':
-          
+      case 'login':
+        setActiveTap(Login)
+        break;
+      // case 'profile':
+      //   setActiveTap(Profile)
+      //   break;
+      case 'register':
+        setActiveTap(Register)
         break;
       case 'cards':
         setActiveTap(Card2)
         break;
       case 'calandar':
         setActiveTap(myCalendar)
-        break;
-      case 'setting':
-
         break;
 
       default:
@@ -38,11 +44,13 @@ function App() {
       <Layout>
         <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
           <div className="logo" />
-          <Menu onClick={switchTap} theme="dark" mode="horizontal" defaultSelectedKeys={['cards']}>
-            <Menu.Item key="profile">Profile</Menu.Item>
+          <Menu onClick={switchTap} theme="dark" mode="horizontal" defaultSelectedKeys={['login']}>
+            <Menu.Item key="login">Login</Menu.Item>
+            <Menu.Item key="register">Register</Menu.Item>
+            {/* <Menu.Item key="profile">Profile</Menu.Item> */}
             <Menu.Item key="calandar">Calandar</Menu.Item>
             <Menu.Item key="cards">Cards</Menu.Item>
-            <Menu.Item key="settings">Settings</Menu.Item>
+
           </Menu>
         </Header>
         <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
---
+++ src/componants/profile/Profile.js
@@ -0,0 +1,35 @@
+import $ from 'jquery';
+import React, { useState, useEffect } from 'react';
+
+const Profile = () => {
+
+    // const [data, setData] = useState();
+    // const url = 'https://reqres.in/api/users?page=1';
+
+    // useEffect(
+    //     () => {
+    //         $.get(url, (response, error) => {
+    //             console.log(response.data);
+    //             setData(response.data);
+    //         })
+    //     }
+    //     , []);
+
+    // if (data != undefined) {
+    //     return (
+    //         <div>
+    //             <h1>This is a List</h1>
+    //             <ul>
+    //                 {/* {data.map((item) => <img  src={item.avatar} />)} */}
+    //                 {data.map((item) => <Card opj={item} />)}
+    //             </ul>
+    //             <button onClick={() => { console.log(data) }}>Add Data</button>
+    //         </div>
+    //     );
+    // } else {
+    //     return ''
+
+    // }
+}
+
+    export default Profile;
